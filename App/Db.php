8e9d6bcbc695412b2330e238fbcd0603aed1067a
dasinua <dasinua@gmail.com>
2016-09-14 15:30:34 +0300

    autoload

---
+++ App/Db.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 14.09.2016
+ * Time: 12:38
+ */
+
+namespace App;
+
+
+class Db
+{
+    function __construct()
+    {
+        echo 'Hello, DB!';
+    }
+}
---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP2</title>
+</head>
+<body>
+<?php
+    require __DIR__ . '/autoload.php';
+
+    $db = new \App\Db();
+?>
+</body>
+</html>
